485fb6a28ae593cef55bd8ac179af449997562c5
mjaber <abujaber44@gmail.com>
2021-01-06 17:29:59 -0500

    Done.

---
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -0,0 +1,15 @@
+class TrainersController < ApplicationController
+    def index
+        trainers = Trainer.all
+        render json: trainers
+      end
+       
+    def show
+        trainer = Trainer.find_by(id: params[:id])
+        if trainer
+            render json: trainer
+        else
+            render json: { message: 'No trainer found with that id' }
+        end
+    end   
+end
