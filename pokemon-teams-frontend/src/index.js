2855aeb1e7756c295351554fce9b39d88ebcf342
Jade Lynn Masker <jadelynn@masker.family>
2021-01-14 17:34:23 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,12 +1,14 @@
 const BASE_URL = "http://localhost:3000";
 const TRAINERS_URL = `${BASE_URL}/trainers`;
 const POKEMONS_URL = `${BASE_URL}/pokemons`;
-const MAIN_TAG = document.getElementsByTagName("main");
+const MAIN_TAG = document.getElementById('main');
 
 function makePokemonIdPokebutton(id) {
 	pokebutton = document.createElement("button");
 	pokebutton.className = `release pokemon-${id}`;
-	return fetch(`${POKEMONS_URL}/${id}`).then(function(pokemon) {
+	return fetch(`${POKEMONS_URL}/${id}`).then(function(response) {
+		return response.json();
+	}).then(function(pokemon) {
 		let pokeAttr = pokemon.data.attributes;
 		pokebutton.innerText = `${pokeAttr.nickname}(${pokeAttr.species})`;
 		return pokebutton;
@@ -26,17 +28,23 @@ function makeTrainerCard(trainer) {
 	addPokebutton.innerText = "Add Pokemon";
 	card.appendChild(addPokebutton);
 
+	let pokebuttonUl = document.createElement("ul");
+	card.appendChild(pokebuttonUl);
+
 	for (let pokemon of trainer.relationships.pokemons.data) {
 		makePokemonIdPokebutton(pokemon.id).then(function(pokebutton) {
-			card.appendChild(pokebutton);
+			let pokebuttonLi = document.createElement("li");
+			pokebuttonLi.appendChild(pokebutton);
+			pokebuttonUl.appendChild(pokebuttonLi);
 		});
 	}
+	return card;
 }
 
 fetch(TRAINERS_URL).then(function(response) {
 	return response.json();
 }).then(function(json) {
 	for (let trainer of json.data) {
-		makeTrainerCard(trainer);
+		MAIN_TAG.appendChild(makeTrainerCard(trainer));
 	}
 });
